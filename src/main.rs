f2fba71cb8bce281c663ac9ffc72b271a6f5e66c
Yannick Meeus <yannick.meeus@gmail.com>
2021-12-31 11:43:47 +0200

    Add json ping response

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,29 @@
 */
 
 use actix_web::{App, HttpServer, Responder, HttpResponse, get};
+use serde::Serialize;
+
+
+#[derive(Serialize)]
+struct PingResponse {
+    data: &'static str,
+}
 
 #[get("/_plain_ping")]
 async fn plain_ping() -> impl Responder {
     HttpResponse::Ok().body("simple_pong")
+}
+#[get("/_json_ping")]
+async fn json_ping() -> impl Responder {
+    HttpResponse::Ok().json(PingResponse { data: "json_pong" })
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(plain_ping)
+            .service(plain_ping) //Registers a handler
+            .service(json_ping)
     })
     .bind("127.0.0.1:8080")?
     .run()
